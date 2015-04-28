e7d78f290cb6e37b8e3f42ae746a8eee0bee4464
Radizzt <Richardthienhuynh@gmail.com>
2015-04-28 17:01:47 -0400

    Added basic posts list template and static data.

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,20 @@
+/**
+ * Created by MAC1 on 15-04-28.
+ */
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];
+Template.postsList.helpers({
+    posts: postsData
+});
