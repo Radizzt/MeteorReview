b0287bd2d00d896a1c5f8392755ac6c1fe1848ba
Radizzt <Richardthienhuynh@gmail.com>
2015-04-29 11:37:28 -0400

    removed 'autopublish' package

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,20 +1,8 @@
 /**
  * Created by MAC1 on 15-04-28.
  */
-var postsData = [
-    {
-        title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
