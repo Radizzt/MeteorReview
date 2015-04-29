3f445fb555f7ed966cc85642811673f5221b63f8
Radizzt <Richardthienhuynh@gmail.com>
2015-04-29 15:46:10 -0400

    Routing to a snigle post page.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -5,6 +5,8 @@ Template.postItem.helpers({
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
+        console.log(this.url);
+        console.log(this.title);
         return a.hostname;
     }
 });
