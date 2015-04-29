a7e1462e33480f516c30f720ab36f024d1ec5670
Radizzt <Richardthienhuynh@gmail.com>
2015-04-29 10:24:36 -0400

    setup a 'domain' helper on the 'postitem'.

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by MAC1 on 15-04-29.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
