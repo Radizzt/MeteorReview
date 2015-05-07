994fd3b57b93d49db93f85530eeaedc3b8f39177
Radizzt <Richardthienhuynh@gmail.com>
2015-05-07 17:18:42 -0400

    Add a spinner to make pagination nicer.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,3 +1,12 @@
 /**
  * Created by MAC1 on 15-04-28.
  */
+Template.postsList.helpers({
+   alert: function(){
+       alert("In else!");
+   },
+
+    log: function(){
+        alert("in If!");
+    }
+});
